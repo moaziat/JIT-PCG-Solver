144e423c30cb544d4a81eded0773d520c6246db3
moaziat <moha.aziat@gmail.com>
2024-11-13 09:46:39 +0100

    convergence test added

--- main/conjugate_gradient.py
+++ main/conjugate_gradient.py
@@ -7,6 +7,12 @@ def conjugate_gradient(A: sparse.csr_matrix, b: np.ndarray, x0: np.ndarray = Non
     #dim of A 
     n = A.shape[0]
 
+    #Assure that the dimentionality of the problem
+    if A.shape != (n, n): 
+        raise  ValueError("dim of must be (n, n)")
+    if b.shape != (n,): 
+        raise ValueError("dim of b must be 1D-array; dim(b) = n")
+    
     #Initial guess
     if x0 is None:
         x = np.zeros(n)
@@ -19,6 +25,8 @@ def conjugate_gradient(A: sparse.csr_matrix, b: np.ndarray, x0: np.ndarray = Non
     #Initial search direction
     p = r.copy()
 
+    # initial residual norm for relative criterion 
+    r_initial_norm = np.sqrt(r.dot(r))
     #initial residual norm squared
     r_norm_sq = r.dot(r)
 
@@ -31,20 +39,35 @@ def conjugate_gradient(A: sparse.csr_matrix, b: np.ndarray, x0: np.ndarray = Non
         opt_step_size = r_norm_sq / p.dot(Ap)
 
         #update solution : takes step of the size of optimal step in direction p
-        x = x + opt_step_size @ p
+        x = x + opt_step_size * p
 
         #update residual
-        r = r - opt_step_size @ Ap
-        new_norm_sq = r.dot(r)
+        r = r - opt_step_size * Ap
+        r_norm_sq_new = r.dot(r)
+
+        '''
+        Checking convergence using relative residual criterion 
+        ||r(i)|| <= tol * ||r(0)|| e.g ||r(i)||/||r(0)|| <= tol
+        '''
+        
+        relative_residual = np.sqrt(r_norm_sq_new) / r_initial_norm
+
+        if relative_residual <= tol: 
+            return x, i, relative_residual
+
+
+       
 
         #conjugacy factor        
-        conj_factor =  new_norm_sq/ r_norm_sq
+        conj_factor =  r_norm_sq_new/ r_norm_sq
 
         #update search direction
         p = r + conj_factor * p 
 
         #update for new iter
-        r_norm_sq = new_norm_sq 
+        r_norm_sq = r_norm_sq_new
+
+    return x, max_iter, relative_residual
 
 
 
